e63f049ae509fc154d807ba20e0c24726ea1cd28
codingeet <geet2136@gmail.com>
2025-02-26 11:42:31 +0530

    digital clock styling updated

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ function startClock () {
         let hours = time.getHours();
         let minutes = time.getMinutes();
         let seconds = time.getSeconds();
-        let ampm = hours >= 12 ? 'pm' : 'am';    
+        let AmPm = hours >= 12 ? 'PM' : 'AM';    
         hours = hours % 12;
         hours = hours ? hours : 12; // the hour '0' should be '12'
         hours = hours < 10 ? "0" + hours : hours;
         minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;  
-        clockDiv.innerHTML = `${hours} : ${minutes} : ${seconds} : ${ampm}`;
+        clockDiv.innerHTML = `${hours} : ${minutes} : ${seconds}  ${AmPm}`;
     }, 1000);
-}
+} 
